6bafe5a84e257a6253f3a1aa56712e6b25a2fc82
Perry Anderson <peryanderson@gmail.com>
2014-10-24 13:20:56 -0500

    landing page further styled

--- public/js/iTrollSoHard.js
+++ public/js/iTrollSoHard.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
   $('h2 a').hover(function(){
   var troll_message = "You think you troll hard?"
+  $( this ).css("font-size", "50px");
+  $( this ).css("display", "inline-block");
   $( this ).text(troll_message);
 },
 function(){
-  var troll_message = "I troll harder"
+  var troll_message = "I troll harder."
   $( this ).text(troll_message);
 }
 )
